cbb16a4151267f624a4f5d91afa3b050839b72f9
Patrick G. Bridges <patrickb@unm.edu>
2022-04-21 17:25:16 -0600

    Fixing the github cmake host config to be able to run the MPI smoke test

--- configs/github/host-config.cmake
+++ configs/github/host-config.cmake
@@ -1,4 +1,5 @@
-# OpenMPI needs an extra argument to run as root
-set(BLT_MPI_COMMAND_APPEND "--allow-run-as-root" CACHE PATH "") 
+# OpenMPI needs an extra argument to run as root and needs to be able
+# to oversubscribe CPUs to pass the MPI smoke test
+set(BLT_MPI_COMMAND_APPEND "--allow-run-as-root --oversubscribe" CACHE PATH "") 
 # No CUDA on github-hosted runners
 set(ENABLE_CUDA "OFF" CACHE PATH "")
